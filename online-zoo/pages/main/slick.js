d9dcedef8acd55f0a1445117065e8cab2cfc84b0
pepsif <pepsi_fack@ukr.net>
2022-10-05 17:35:51 +0300

    feat: add slider in Testmonials section

--- online-zoo/pages/main/slick.js
+++ online-zoo/pages/main/slick.js
@@ -1,9 +1,22 @@
+// ------gallery-slider----
+
 $(document).ready(function(){
     $('.gallery-block').slick({
    
       infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1
+
+    });
+  });
+
+  // -----TESTMONIALS------------
+$(document).ready(function(){
+    $('.review-block').slick({
+   arrows: false,
+      infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4
 
     });
   });

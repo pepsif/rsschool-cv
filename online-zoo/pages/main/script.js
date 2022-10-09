578b6fd1bba917b1aae735af36c50cf17ddc9f09
pepsif <pepsi_fack@ukr.net>
2022-10-09 12:43:02 +0300

    feat: edit random order in cards block

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,4 +1,26 @@
+//   --RANDOM-CARDS-IN-SLIDER--GENERATOR----
+const slickButtonPrev = document.getElementsByClassName('.slick-prev');
+
+
+const slickButtonNext = document.querySelector('.slick-next');
+
+console.log(slickButtonPrev)
+
 const cardsBlock = document.querySelector('.cards-block');
 const cards = document.querySelectorAll('.card');
+const arrCards = Array.from(cardsBlock.children);
+const randomOrder = arrCards.forEach(elem => {
+  elem.style.order = getRandomInt(1,7)
+        // console.log(elem)
+})
 
-console.log(cardsBlock)
+// console.log( getRandomInt(1,7))
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  }
+
+
+//   ------------------------------------
